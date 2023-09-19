751162cc73a57bdd2e3d58b99c6087915b1f8471
matheus-ferreira <matheus8142@hotmail.com>
2023-09-19 13:58:24 -0300

    Adicionando bootstrap no profile img

--- app/views/header.php
+++ app/views/header.php
@@ -91,7 +91,7 @@
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$nome?></span>
-                        <img class="img-profile rounded-circle"
+                        <img class="img-profile rounded-circle object-fit-cover border rounded"
                             src="<?=$foto?>">
                     </a>
                     <!-- Dropdown - User Information -->
