2a36ca32f0bfb9416e4928354d2c0118e8e73219
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2023-10-01 13:35:05 -0300

    removendo debug

--- app/controllers/studioController.php
+++ app/controllers/studioController.php
@@ -4,14 +4,6 @@ use models\Sistema as Model;
 class studioController extends controllerHelper{
     public function viewIndex(){
         $admin = $this->isLogged();
-
-        $model = new Model();
-        echo '<pre>'; 
-        print_r($model->get());
-        echo '<br> '.__CLASS__.'| Linha: '.__LINE__. '<br>';
-        echo '<pre>'; 
-        exit;
-
         $data['component'] = $admin;
 
         $this->loadView('studio', $data);
